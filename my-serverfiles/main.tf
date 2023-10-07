54dd9b25ebadef074b2ff5c64bebad548eef3a6d
belosheabhijeet <113359229+belosheabhijeet@users.noreply.github.com>
2023-10-07 09:33:27 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
-  key_name = "Awskeypair"
+  key_name = "abhijeetkeypair"
   vpc_security_group_ids= ["sg-038b8a41bfb3a0d60"] 
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./Awskeypair.pem")
+    private_key = file("./abhijeetkeypair.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
